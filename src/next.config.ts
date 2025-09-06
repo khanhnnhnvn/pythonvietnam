c73f9f99432108c3346a0805dfbb8c830c56b6bc
Khanh Nguyen Ngoc <khanhnnhnvn@gmail.com>
2025-09-06 07:21:24 +0000

    The app isn't starting. Please investigate what could be wrong based on

---
+++ src/next.config.ts
@@ -0,0 +1,40 @@
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https' as const,
+        hostname: 'khanhnn.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  // The value below is a workaround for development given that the
+  // server is proxied in the editor.
+  // In a production deployment, this should be removed.
+  experimental: {},
+  allowedDevOrigins: ['https://*.cloudworkstations.dev'],
+};
+
+export default nextConfig;
